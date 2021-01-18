78638b1aebccfecb78e16637c9b9c402827e36e7
Hi-Timofey <Katkov0103@yandex.ru>
2021-01-18 19:33:10 +1000

    Added potential new blocks in game ( teleport, trap)

--- blocks.py
+++ blocks.py
@@ -1,12 +1,23 @@
 from pygame import sprite, image
+import pyganim
 from pygame.locals import Color
 from pygame.rect import Rect
 from pygame.surface import Surface
 from random import randint
 
+from utils import get_data_path
+
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = "#FF6262"
+
+PLATFORM_WIDTH = 32
+PLATFORM_HEIGHT = 32
+PLATFORM_COLOR = "#FF6262"
+# ANIMATION_BLOCKTELEPORT = [
+#             (get_data_path("sjf2r.png", 'img')),
+#             (get_data_path("sjf2r.png", 'img'))]
+COLOR = "#888888"
 
 
 class Platform(sprite.Sprite):
@@ -16,3 +27,30 @@ class Platform(sprite.Sprite):
         self.image = image
         # self.image.fill(Color(PLATFORM_COLOR))
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+
+
+class BlockDie(Platform):
+    def __init__(self, x, y):
+        img = image.load(get_data_path('ship.png', 'img')).convert_alpha()
+        self.image = Surface(
+            (PLATFORM_WIDTH, PLATFORM_HEIGHT),
+            pygame.SRCALPHA)
+        Platform.__init__(self, x, y)
+
+        self.image = img
+
+
+class BlockTeleport(Platform):
+    def __init__(self, x, y, goX, goY):
+        Platform.__init__(self, x, y)
+        self.goX = goX  # координаты назначения перемещения
+        self.goY = goY  # координаты назначения перемещения
+        boltAnim = []
+        for anim in ANIMATION_BLOCKTELEPORT:
+            boltAnim.append((anim, 1))
+        self.boltAnim = pyganim.PygAnimation(boltAnim)
+        self.boltAnim.play()
+
+    def update(self):
+        self.image.fill(Color(PLATFORM_COLOR))
+        self.boltAnim.blit(self.image, (0, 0))
