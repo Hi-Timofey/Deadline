746e6a3371bca4845d6bff2be44bad8154433051
Hi-Timofey <Katkov0103@yandex.ru>
2021-01-18 15:52:28 +1000

    Added some functional to utils for easier working with src

--- utils.py
+++ utils.py
@@ -7,5 +7,19 @@ def get_data_path(name, subfolder):
     fullname = os.path.join(first_part, name)
 
     if not os.path.isfile(fullname):
-        raise ValueError(f"Файл с изображением '{fullname}' не найден")
+        raise ValueError(f"Source file '{name}' not found ({fullname})")
+    return fullname
+
+def get_font_path(name):
+    fullname = os.path.join(os.path.join('data', 'font'), name)
+
+    if not os.path.isfile(fullname):
+        raise ValueError(f"File with font '{name}' not found ({fullname})")
+    return fullname
+
+def get_img_path(name):
+    fullname = os.path.join(os.path.join('data', 'img'), name)
+
+    if not os.path.isfile(fullname):
+        raise ValueError(f"File with imgae '{name}' not found ({fullname})")
     return fullname
