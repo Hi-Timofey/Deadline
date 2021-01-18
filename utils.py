66da95f1569c069a83432decbed6ff1feb109e0f
Hi-Timofey <Katkov0103@yandex.ru>
2021-01-18 18:39:07 +1000

    added function for easify image loading

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+from pygame import image
 
 
 def get_data_path(name, subfolder):
@@ -10,6 +11,7 @@ def get_data_path(name, subfolder):
         raise ValueError(f"Source file '{name}' not found ({fullname})")
     return fullname
 
+
 def get_font_path(name):
     fullname = os.path.join(os.path.join('data', 'font'), name)
 
@@ -17,9 +19,15 @@ def get_font_path(name):
         raise ValueError(f"File with font '{name}' not found ({fullname})")
     return fullname
 
+
 def get_img_path(name):
     fullname = os.path.join(os.path.join('data', 'img'), name)
 
     if not os.path.isfile(fullname):
         raise ValueError(f"File with imgae '{name}' not found ({fullname})")
     return fullname
+
+
+def loadify(imgname):
+    ''' Удобная загрузка изображений'''
+    return image.load(imgname).convert_alpha()
