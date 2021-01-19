acc53ae11c0b9d3705bb1a28b4fe9a56fa6eb263
Hi-Timofey <Katkov0103@yandex.ru>
2021-01-19 17:01:14 +1000

    Shorted logic of gravity and non gravity player mode

--- player.py
+++ player.py
@@ -77,21 +77,22 @@ class Player(sprite.Sprite):
             boltAnim.append((anim, ANIMATION_DELAY))
 
     def update(self, left, right, up, down, platforms):
+        if left:
+            self.xvel = -MOVE_SPEED  # Лево = x- n
+            self.image.fill(Color(COLOR))
+            if up:  # для прыжка влево есть отдельная анимация
+                self.boltAnimJumpLeft.blit(self.image, (0, 0))
+            else:
+                self.boltAnimLeft.blit(self.image, (0, 0))
+        if right:
+            self.xvel = MOVE_SPEED  # Право = x + n
+            self.image.fill(Color(COLOR))
+            if up:
+                self.boltAnimJumpRight.blit(self.image, (0, 0))
+            else:
+                self.boltAnimRight.blit(self.image, (0, 0))
+
         if self.gravity:
-            if left:
-                self.xvel = -MOVE_SPEED  # Лево = x- n
-                self.image.fill(Color(COLOR))
-                if up:  # для прыжка влево есть отдельная анимация
-                    self.boltAnimJumpLeft.blit(self.image, (0, 0))
-                else:
-                    self.boltAnimLeft.blit(self.image, (0, 0))
-            if right:
-                self.xvel = MOVE_SPEED  # Право = x + n
-                self.image.fill(Color(COLOR))
-                if up:
-                    self.boltAnimJumpRight.blit(self.image, (0, 0))
-                else:
-                    self.boltAnimRight.blit(self.image, (0, 0))
             if up:
                 if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
                     self.yvel = -JUMP_POWER
@@ -104,28 +105,7 @@ class Player(sprite.Sprite):
                     self.boltAnimStay.blit(self.image, (0, 0))
             if not self.onGround:
                 self.yvel += GRAVITY
-
-            self.onGround = False  # Мы не знаем, когда мы на земле((
-            self.rect.y += self.yvel
-            self.collide(0, self.yvel, platforms)
-
-            self.rect.x += self.xvel  # переносим свои положение на xvel
-            self.collide(self.xvel, 0, platforms)
         else:
-            if left:
-                self.xvel = -MOVE_SPEED  # Лево = x- n
-                self.image.fill(Color(COLOR))
-                if up:  # для прыжка влево есть отдельная анимация
-                    self.boltAnimJumpLeft.blit(self.image, (0, 0))
-                else:
-                    self.boltAnimLeft.blit(self.image, (0, 0))
-            if right:
-                self.xvel = MOVE_SPEED  # Право = x + n
-                self.image.fill(Color(COLOR))
-                if up:
-                    self.boltAnimJumpRight.blit(self.image, (0, 0))
-                else:
-                    self.boltAnimRight.blit(self.image, (0, 0))
             if up:
                 self.yvel = -MOVE_SPEED
                 self.image.fill(Color(COLOR))
@@ -140,12 +120,13 @@ class Player(sprite.Sprite):
                 self.image.fill(Color(COLOR))
                 self.boltAnimStay.blit(self.image, (0, 0))
 
-            # self.onGround = False  # Мы не знаем, когда мы на земле((
-            self.rect.y += self.yvel
-            self.collide(0, self.yvel, platforms)
+        if self.gravity:
+            self.onGround = False  # Мы не знаем, когда мы на земле((
+        self.rect.y += self.yvel
+        self.collide(0, self.yvel, platforms)
 
-            self.rect.x += self.xvel  # переносим свои положение на xvel
-            self.collide(self.xvel, 0, platforms)
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+        self.collide(self.xvel, 0, platforms)
 
     def draw(self, screen):  # Выводим себя на экран
         screen.blit(self.image, (self.rect.x, self.rect.y))
@@ -155,33 +136,17 @@ class Player(sprite.Sprite):
             if sprite.collide_rect(
                     self, p):  # если есть пересечение платформы с игроком
 
-                if self.gravity:
-                    if xvel > 0:  # если движется вправо
-                        self.rect.right = p.rect.left  # то не движется вправо
-
-                    if xvel < 0:  # если движется влево
-                        self.rect.left = p.rect.right  # то не движется влево
-
-                    if yvel > 0:  # если падает вниз
-                        self.rect.bottom = p.rect.top  # то не падает вниз
+                if xvel > 0:  # если движется вправо
+                    self.rect.right = p.rect.left  # то не движется вправо
+                if xvel < 0:  # если движется влево
+                    self.rect.left = p.rect.right  # то не движется влево
+                if yvel > 0:  # если падает вниз
+                    self.rect.bottom = p.rect.top  # то не падает вниз
+                    if self.gravity:
                         self.onGround = True  # и становится на что-то твердое
                         self.yvel = 0  # и энергия падения пропадает
 
-                    if yvel < 0:  # если движется вверх
-                        self.rect.top = p.rect.bottom  # то не движется вверх
+                if yvel < 0:  # если движется вверх
+                    self.rect.top = p.rect.bottom  # то не движется вверх
+                    if self.gravity:
                         self.yvel = 0  # и энергия прыжка пропадает
-                else:
-                    if xvel > 0:  # если движется вправо
-                        self.rect.right = p.rect.left  # то не движется вправо
-
-                    if xvel < 0:  # если движется влево
-                        self.rect.left = p.rect.right  # то не движется влево
-
-                    if yvel > 0:  # если падает вниз
-                        self.rect.bottom = p.rect.top  # то не падает вниз
-                        # self.onGround = True  # и становится на что-то твердое
-                        # self.yvel = 0  # и энергия падения пропадает
-
-                    if yvel < 0:  # если движется вверх
-                        self.rect.top = p.rect.bottom  # то не движется вверх
-                        # self.yvel = 0  # и энергия прыжка пропадает
