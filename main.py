6443856966083e7f1fd46e419308d6893594de8d
Hi-Timofey <Katkov0103@yandex.ru>
2021-01-21 22:04:15 +1000

    Попытка реализации коллбэк функционала для паузы и переключения уровней В лэвл идёт функция, она переключает контекст игры, игра выдаёт пользователю паузу/другой лэвэл/аллаха/etc

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame
 import pygame_menu
 from db_related import Scores
 from utils import *
+from create_level import create_level
 
 
 pygame.init()
@@ -17,7 +18,7 @@ window_size = window_width, window_height = 800, 800
 game_size = g_width, g_height = 800, 800
 
 main_menu = None
-game = False
+credits, game, loading = False, False, False
 surface = pygame.display.set_mode(window_size)
 
 background_image = pygame_menu.baseimage.BaseImage(
@@ -30,7 +31,36 @@ def draw_background():
     background_image.draw(surface)
 
 
-def start_end_credits():
+def loaded():
+    global loading
+    loading = False
+
+
+def game_over():
+    global game
+    game = False
+
+
+def start_the_game_from_menu():
+    global game
+    global loading
+    game = True
+    loading = True
+
+
+def main():
+    global game
+    global loading
+    pygame.mixer.music.load(get_data_path('menu_theme.wav', 'music'))
+    pygame.mixer.music.play(-1)
+
+    main_menu = pygame_menu.Menu(300, 300, 'Fire Dungeon',
+                                 theme=pygame_menu.themes.THEME_BLUE)
+    scores_menu = Scores(window_width, window_height)
+    main_menu.add_button('Play', start_the_game_from_menu)
+
+    main_menu.add_button('Scores', scores_menu.menu)
+
     credit_list = [
         "CREDITS - The Departed",
         " ",
@@ -40,32 +70,10 @@ def start_end_credits():
         "Mark Wahlberg - Dignam",
         "Martin Sheen - Queenan"]
     c = Credits(credit_list, surface, 'Sigma Five.otf')
-    c.main()
-
-
-def start_the_game():
-    game = True
-    fd = fire_dungeon.FireDungeon(g_width,g_height)
-    surface.fill((0,0,0))
-    fd.run_game(gravity=False)
-
-
-def main():
-    pygame.mixer.music.load(get_data_path('menu_theme.wav', 'music'))
-    pygame.mixer.music.play(-1)
-
-    main_menu = pygame_menu.Menu(300, 300, 'Fire Dungeon',
-                                 theme=pygame_menu.themes.THEME_BLUE)
-    scores_menu = Scores(window_width, window_height)
-    main_menu.add_button('Play', start_the_game)
-
-    main_menu.add_button('Scores', scores_menu.menu)
-    main_menu.add_button('About', start_end_credits)
+    main_menu.add_button('About', c.main)
     main_menu.add_button('Quit', pygame_menu.events.EXIT)
 
     while True:
-
-        draw_background()
 
         events = pygame.event.get()
         for event in events:
@@ -74,8 +82,26 @@ def main():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 pass
 
-        main_menu.mainloop(surface, draw_background, fps_limit=FPS)
+            if not game:
+                main_menu.update(events)
 
+        if not game:
+            draw_background()
+            if not loading:
+                main_menu.draw(surface)
+            if loading:
+                surface.fill((0, 0, 0))
+        else:
+            # if loading:
+            surface.fill((0, 0, 0))
+            level = create_level(31, 31, 1, callback=loaded)
+            if not loading:
+                fd = fire_dungeon.FireDungeon(
+                    level, g_width, g_height, game_over_func=game_over)
+
+                fd.run_game(gravity=False)
+
+        # Credits(credit_list, surface, 'Sigma Five.otf').main()
         pygame.display.update()
 
 
