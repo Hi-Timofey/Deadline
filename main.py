882853ce884b1a5411fce19bfc7528770c203460
Hi-Timofey <Katkov0103@yandex.ru>
2021-01-28 21:37:20 +1000

    Storing scores of the player with time and value Can load them only on restart

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ clock = pygame.time.Clock()
 window_size = window_width, window_height = 800, 800
 game_size = g_width, g_height = 800, 800
 
+surface = pygame.display.set_mode(window_size)
+
 main_menu = None
 credits, game = False, False
-surface = pygame.display.set_mode(window_size)
 
 background_image = pygame_menu.baseimage.BaseImage(
     image_path=get_data_path('background.png', 'img'))
@@ -36,7 +37,7 @@ def draw_background():
 
 def start_the_game_from_menu():
 
-    global game
+    global game, main_menu
     game = True
 
     # TODO Music
@@ -63,13 +64,15 @@ def start_the_game_from_menu():
             mv_extra_multi=player_mv_extra,
             gravity=gravity)
 
+        level = create_level(level_width, level_height, LEVEL)
         fire_dungeon_lvl = fire_dungeon.FireDungeon(
-            create_level(level_width, level_height, LEVEL),
+            level,
             player, g_width, g_height, fire_speed)
         result = fire_dungeon_lvl.run_game(False)
         print(result)
         del fire_dungeon_lvl
         del player
+        create_main_menu()
         if result == 1:
             game = False
         elif result == 3:
@@ -85,11 +88,13 @@ def start_the_game_from_menu():
             game = True
         elif result == 2:
             game = False
+            scores_menu.add_score(LEVEL * 100)
     if not game:
         pygame.mixer.Channel(3).unpause()
 
 
 def main():
+    global main_menu
     pygame.mixer.Channel(3).set_volume(0.75)
     pygame.mixer.Channel(3).play(
         pygame.mixer.Sound(
@@ -97,26 +102,6 @@ def main():
                 'menu_theme.wav',
                 'music')),
         loops=-1)
-    # pygame.mixer.music.load(get_data_path('menu_theme.wav', 'music'))
-
-    main_menu = pygame_menu.Menu(300, 300, 'Fire Dungeon',
-                                 theme=pygame_menu.themes.THEME_BLUE)
-    scores_menu = Scores(window_width, window_height)
-    main_menu.add_button('Play', start_the_game_from_menu)
-
-    main_menu.add_button('Scores', scores_menu.menu)
-
-    credit_list = [
-        "CREDITS - The Departed",
-        " ",
-        "Leonardo DiCaprio - Billy",
-        "Matt Damon - Colin Sullivan",
-        "Jack Nicholson - Frank Costello",
-        "Mark Wahlberg - Dignam",
-        "Martin Sheen - Queenan"]
-    c = Credits(credit_list, surface, 'Sigma Five.otf')
-    main_menu.add_button('About', c.main)
-    main_menu.add_button('Quit', pygame_menu.events.EXIT)
 
     while True:
 
@@ -138,5 +123,27 @@ def main():
         pygame.display.update()
 
 
+def create_main_menu():
+
+    global main_menu, scores_menu
+    scores_menu = Scores(int(window_width/1.3), int(window_height/1.3))
+    main_menu = pygame_menu.Menu(300, 300, 'Fire Dungeon',
+                                 theme=pygame_menu.themes.THEME_BLUE)
+    main_menu.add_button('Play', start_the_game_from_menu)
+    main_menu.add_button('Scores', scores_menu.menu)
+    credit_list = [
+        "CREDITS - The Departed",
+        " ",
+        "Leonardo DiCaprio - Billy",
+        "Matt Damon - Colin Sullivan",
+        "Jack Nicholson - Frank Costello",
+        "Mark Wahlberg - Dignam",
+        "Martin Sheen - Queenan"]
+    c = Credits(credit_list, surface, 'Sigma Five.otf')
+    main_menu.add_button('About', c.main)
+    main_menu.add_button('Quit', pygame_menu.events.EXIT)
+
+
 if __name__ == '__main__':
+    create_main_menu()
     main()
